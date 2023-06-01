98c4ece9216b1b021f7f41c8be07f6f97b754cf0
David Hernandez <dhxer.solutions@gmail.com>
2023-06-01 15:42:39 -0500

    [RIMAK-DAVE] Add process - get insights

--- getInsights.js
+++ getInsights.js
@@ -33,55 +33,61 @@ module.exports.handler = async (event) => {
             }
         ]).toArray();
     }else{
-        res = await client.collection("insights").aggregate([
-            {
-                $match: { product: body.product }
-            },
-            {
-                $project: {
-                    sentiment: 1,
-                    product: 1,
-                    day: { $dayOfMonth: { $toDate: "$createdAt" } },
-                    month: { $month: { $toDate: "$createdAt" } },
-                    year: { $year: { $toDate: "$createdAt" } }
-                }
-            },
-            {
-                $group: {
-                    _id: {
-                        day: "$day",
-                        month: "$month",
-                        year: "$year",
-                        product: "$product",
-                        sentiment: "$sentiment"
-                    },
-                    count: { $sum: 1 }
-                }
-            },
-            {
-                $group: {
-                    _id: {
-                        day: "$_id.day",
-                        month: "$_id.month",
-                        year: "$_id.year",
-                        product: "$_id.product"
-                    },
-                    sentiments: {
-                        $push: {
-                            sentiment: "$_id.sentiment",
-                            count: "$count"
+        console.log("pass")
+        try {
+            res = await client.collection("insights").aggregate([
+                {
+                    $match: { product: body.product }
+                },
+                {
+                    $project: {
+                        sentiment: 1,
+                        product: 1,
+                        day: { $dayOfMonth: { $toDate: "$createdAt" } },
+                        month: { $month: { $toDate: "$createdAt" } },
+                        year: { $year: { $toDate: "$createdAt" } }
+                    }
+                },
+                {
+                    $group: {
+                        _id: {
+                            day: "$day",
+                            month: "$month",
+                            year: "$year",
+                            product: "$product",
+                            sentiment: "$sentiment"
+                        },
+                        count: { $sum: 1 }
+                    }
+                },
+                {
+                    $group: {
+                        _id: {
+                            day: "$_id.day",
+                            month: "$_id.month",
+                            year: "$_id.year",
+                            product: "$_id.product"
+                        },
+                        sentiments: {
+                            $push: {
+                                sentiment: "$_id.sentiment",
+                                count: "$count"
+                            }
                         }
                     }
+                },
+                {
+                    $sort: {
+                        "_id.year": 1,
+                        "_id.month": 1,
+                        "_id.day": 1
+                    }
                 }
-            },
-            {
-                $sort: {
-                    "_id.year": 1,
-                    "_id.month": 1,
-                    "_id.day": 1
-                }
-            }
-        ]).toArray();
+            ]).toArray();
+
+        }catch (e){
+            console.log("ERR" , e)
+        }
     }
 
 
@@ -100,6 +106,7 @@ module.exports.handler = async (event) => {
 async function getClientMDB(dbName = `${process.env.MONGODB_DBNAME}`) {
     const mongo_uri = `${process.env.MONGO_URI}`;
 
+    console.log("mongo_uri", mongo_uri, "db", `${process.env.MONGODB_DBNAME}`);
     const client = await mongodb.MongoClient.connect(mongo_uri);
 
     const db = await client.db(dbName);
