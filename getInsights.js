f39e9e9a3e0a3c713c444e3a729c13b5fd33e143
David Hernandez <dhxer.solutions@gmail.com>
2023-06-01 13:08:17 -0500

    [RIMAK-DAVE] Add process - get insights

--- getInsights.js
+++ getInsights.js
@@ -7,7 +7,56 @@ module.exports.handler = async (event) => {
     const body = JSON.parse(event.body);
 
     const client = await getClientMDB();
-    const res = await client.collection("insights").find({product: body.product}).toArray();
+    const res = await client.collection("insights").aggregate([
+        {
+            $match: { product: body.product }
+        },
+        {
+            $project: {
+                sentiment: 1,
+                product: 1,
+                day: { $dayOfMonth: { $toDate: "$createdAt" } },
+                month: { $month: { $toDate: "$createdAt" } },
+                year: { $year: { $toDate: "$createdAt" } }
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    day: "$day",
+                    month: "$month",
+                    year: "$year",
+                    product: "$product",
+                    sentiment: "$sentiment"
+                },
+                count: { $sum: 1 }
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    day: "$_id.day",
+                    month: "$_id.month",
+                    year: "$_id.year",
+                    product: "$_id.product"
+                },
+                sentiments: {
+                    $push: {
+                        sentiment: "$_id.sentiment",
+                        count: "$count"
+                    }
+                }
+            }
+        },
+        {
+            $sort: {
+                "_id.year": 1,
+                "_id.month": 1,
+                "_id.day": 1
+            }
+        }
+    ]).toArray();
+
 
     return {
         statusCode: 200,
