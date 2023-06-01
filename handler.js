238fc83b8b26471ecf6e9e21f966a0336a458ce9
Brayan Portugal <brayan.portugal@kushkipagos.com>
2023-06-01 10:45:48 -0500

    Lambda Sentiment

--- handler.js
+++ handler.js
@@ -1,18 +1,39 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+const AWS = require('aws-sdk');
+const comprehend = new AWS.Comprehend();
+
+
+module.exports.sentimentAnalisys = async (event) => {
+
+  const body = JSON.parse(event.body);
+
+  // Parámetros para el análisis de sentimiento
+  const params = {
+    LanguageCode: 'es',
+    Text: body.text
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  return comprehend.detectSentiment(params).promise().then(
+    async data => {
+      const word = data.Sentiment.toLowerCase()
+      const firstLetter = word.charAt(0)
+      const firstLetterCap = firstLetter.toUpperCase()
+      const remainingLetters = word.slice(1)
+      const capitalizedWord = firstLetterCap + remainingLetters
+
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(
+          {
+            sentiment:data.Sentiment,
+            sentimentPercentage:data.SentimentScore[capitalizedWord]
+          },
+          null,
+          2
+        ),
+      };
+    }
+  )
 };
