2461511ed0bff279f4195e9fa7b5f6a862e3ea27
David Hernandez <dhxer.solutions@gmail.com>
2023-06-02 10:49:20 -0500

    fix avg

--- src/service/GetInsightsService.js
+++ src/service/GetInsightsService.js
@@ -20,7 +20,7 @@ async function getInsights(event) {
                         month: {$month: {$toDate: "$createdAt"}},
                         year: {$year: {$toDate: "$createdAt"}}
                     },
-                    averageValue: "$indicator.value",
+                    averageValue: {$avg: "$indicator.value"},
                 }
             },
             {
